808705be179f4b313ceb6ad2b5e0c698598c4e48
Ergin Turk <ergin@synaps.com>
2025-02-17 22:17:19 +0100

    Create CozyEvent.ts

---
+++ src/core/CozyEvent.ts
@@ -0,0 +1,63 @@
+import { EventHandler } from './types/EventHandler';
+
+class CozyEvent {
+  private _events: Map<string, EventHandler[]> = new Map();
+
+  constructor(useMicroTask: boolean = false) {
+    this.emit = useMicroTask ? this._emitAsMicroTask : this._emit;
+  }
+
+  public on(event: string, handler: EventHandler): void {
+    if (!this._events.has(event)) {
+      this._events.set(event, []);
+    }
+    this._events.get(event)!.push(handler);
+  }
+
+  public once(event: string, handler: EventHandler): void {
+    const onceWrapper: EventHandler = (...args) => {
+      this.off(event, onceWrapper);
+      handler(...args);
+    };
+    this.on(event, onceWrapper);
+  }
+
+  public off(event: string, handler: EventHandler): void {
+    if (this._events.has(event)) {
+      this._events.set(
+        event,
+        this._events.get(event)!.filter((h) => h !== handler)
+      );
+    }
+  }
+
+  public removeAllListeners(event?: string): void {
+    if (event) {
+      this._events.delete(event);
+    } else {
+      this._events.clear();
+    }
+  }
+
+  public emit(event: string, ...args: any[]): void {}
+
+  private _emit(event: string, ...args: any[]): void {
+    if (this._events.has(event)) {
+      this._events.get(event)!.forEach((handler) => handler(...args));
+    }
+  }
+
+  private _emitAsMicroTask(event: string, ...args: any[]): void {
+    if (this._events.has(event)) {
+      queueMicrotask(() => {
+        this._events.get(event)!.forEach((handler) => handler(...args));
+      });
+    }
+  }
+
+  public destroy() {
+    this.removeAllListeners();
+  }
+}
+
+export { CozyEvent };
